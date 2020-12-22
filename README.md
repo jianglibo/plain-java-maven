ce7db214d118aad34064212670a7240153eb8f60
libo <jianglibo@gmail.com>
2020-12-22 11:55:18 +0800

    add other execute method.

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 ```bash
 
-java -cp ".\target\java-plain-1.0-SNAPSHOT.jar;.\target\lib\*" me.resp.App
-
+java -cp ".\target\java-plain-1.0-SNAPSHOT.jar;.\target\lib\*" me.resp.App cmd "dir"
 java -cp ./target/java-plain-1.0-SNAPSHOT.jar:./target/lib/* me.resp.App
 java -cp ../plain-java-maven/target/java-plain-1.0-SNAPSHOT.jar:../plain-java-maven/target/lib/* me.resp.App
 
--- src/main/java/me/resp/App.java
+++ src/main/java/me/resp/App.java
@@ -10,44 +10,61 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * Hello world!
  *
+ * ssh root@104.238.149.23 ls -l ~
  */
 @Slf4j
-public class App
-{
-    public static void printout(Object o) {
-        System.out.println(o);
+public class App {
+
+  public static void printout(Object o) {
+    System.out.println(o);
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    ProcessBuilder builder = new ProcessBuilder();
+    List<String> commands = new ArrayList<>();
+
+    if ("cmd".equals(args[0])) {
+      commands.add("cmd.exe");
+      commands.add("/c");
+      commands.add(args[1]);
+    } else if ("sh".equals(args[0])) {
+      commands.add("sh");
+      commands.add("-c");
+      commands.add(args[1]);
+    } else if ("bash".equals(args[0])) {
+      commands.add("bash");
+      commands.add("-c");
+      commands.add(args[1]);
+    } else {
+      for (Entry<String, String> entry : builder.environment().entrySet()) {
+        log.info(entry.getKey() + "=" + entry.getValue());
+      }
+
+      commands.add("ansible-playbook");
+      commands.add("--vault-id");
+      commands.add("hetzner@/opt/respme/hetzner.pwd");
+
+      commands.add("--extra-vars");
+      String extraVars = "{\"vpn_server_ip\": \"" + args[0]
+          + "\", \"max_conf\": 250, \"provider\": \"vultr\", \"continent\": \"ASIA\"}";
+      printout(extraVars);
+      commands.add(extraVars);
+
+      // Add play book name at last.
+      commands.add("install-algo-to.yml");
+
+      commands.add("-vvv");
     }
-    public static void main( String[] args ) throws IOException, InterruptedException {    ProcessBuilder builder = new ProcessBuilder();
-        List<String> commands = new ArrayList<>();
-
-        for(Entry<String, String> entry: builder.environment().entrySet()) {
-            log.info(entry.getKey() + "=" + entry.getValue());
-        }
-
-        commands.add("ansible-playbook");
-        commands.add("--vault-id");
-        commands.add("hetzner@/opt/respme/hetzner.pwd");
-
-
-        commands.add("--extra-vars");
-        String extraVars = "{\"vpn_server_ip\": \"" + args[0] + "\", \"max_conf\": 250, \"provider\": \"vultr\", \"continent\": \"ASIA\"}";
-        printout(extraVars);
-        commands.add(extraVars);
-
-        // Add play book name at last.
-        commands.add("install-algo-to.yml");
-
-        commands.add("-vvv");
-        builder.command(commands);
-        builder.redirectErrorStream(true);
+    builder.command(commands);
+    builder.redirectErrorStream(true);
 //        builder.directory(new File(getWorkingDirectory()));
-        Process process = builder.start();
-        StreamGobbler streamGobbler =
-            new StreamGobbler(process.getInputStream(), line -> {
-                printout(line);
-            });
-        Executors.newSingleThreadExecutor().submit(streamGobbler);
-        int exit =  process.waitFor();
-        printout( exit );
-    }
+    Process process = builder.start();
+    StreamGobbler streamGobbler =
+        new StreamGobbler(process.getInputStream(), line -> {
+          printout(line);
+        });
+    Executors.newSingleThreadExecutor().submit(streamGobbler);
+    int exit = process.waitFor();
+    printout(exit);
+  }
 }
